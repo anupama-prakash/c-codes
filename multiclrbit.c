5ab27e99e28a44b99b69a03c83adf63b71a58500
anupama-prakash <112626150+anupama-prakash@users.noreply.github.com>
2022-09-02 15:51:26 +0530

    Add files via upload

---
+++ multiclrbit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	unsigned int  num,pos,val,n;
+	printf("enter the number:");
+	scanf("%x",&num);
+	printf("enter the position:");
+	scanf("%d",&pos);
+	printf("enter the number of bits:");
+	scanf("%d",&n);
+	val=((1<<n)-1);
+	num=num&~(val<<pos);
+	printf("0X%x",num);
+	return 0;
+}
