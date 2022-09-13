79cc4bbe6f8419bbe0d784a89dfdb5354611117e
anupama-prakash <112626150+anupama-prakash@users.noreply.github.com>
2022-09-13 15:03:59 +0530

    Add files via upload

---
+++ call.cpp
@@ -0,0 +1,8 @@
+#include <cstdlib> // Declare "system()"
+#include<stdio.h>
+using namespace std;
+int main(int argc,char *argv[]) {
+	char buf[80];
+	sprintf(buf,"%s%s%s%s%s","ifconfig"," ",argv[1]," ",argv[2]);
+	system(buf);
+} ///:~
---
+++ hellonew.cpp
@@ -0,0 +1,7 @@
+#include<iostream> // Stream declarations
+//using namespace std;
+int main()
+{
+	std::cout << "Hello, World! I am "
+ << 8 << " Today!" << std::endl;
+}
